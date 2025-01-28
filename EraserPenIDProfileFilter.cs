0854718c95546152059a6a93f71e569b1db1268c
nahkd123 <nahkd123@gmail.com>
2025-01-28 15:21:37 +0700

    Add eraser filter

---
+++ EraserPenIDProfileFilter.cs
@@ -0,0 +1,27 @@
+﻿using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+
+namespace nahkd123.PenIDProfile
+{
+    [PluginName("Pen ID profile - Eraser")]
+    public class EraserPenIDProfileFilter : BasePenIDProfileFilter
+    {
+        [Property("Pen IDs"), DefaultPropertyValue(""), ToolTip("A comma-separated list of pen IDs to match")]
+        public string PenIDs { get; set; } = string.Empty;
+        public string[] PenIDArray => PenIDs.Split(",");
+        public bool EraserMode { get; private set; } = false;
+
+        public override event Action<IDeviceReport>? Emit;
+
+        public override void Consume(string penId, IDeviceReport value)
+        {
+            if (value is IEraserReport eraserReport) eraserReport.Eraser |= EraserMode;
+            Emit?.Invoke(value);
+        }
+
+        public override void OnPenIDChanged(string penId)
+        {
+            EraserMode = PenIDArray.Contains(penId);
+        }
+    }
+}
